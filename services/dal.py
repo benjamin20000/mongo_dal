0eccc7b32ddfb0874ba117e3f54f7fa32642eaee
benjamin <2000beni770@gmail.com>
2025-08-19 17:18:08 +0300

    add first init func

--- services/dal.py
+++ services/dal.py
@@ -1,1 +1,11 @@
-print("hi")
+import pymongo
+
+
+def init_db_and_collection():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["enemy_soldiers"] ## init the db
+    mycol = mydb["soldier_details"]  ##init the collection
+    ##create the first soldier
+    first_soldier = { "first_name": "John","last_name":"John","phone_number":123, "rank":1 }
+    ##insert the first soldier
+    mycol.insert_one(first_soldier)

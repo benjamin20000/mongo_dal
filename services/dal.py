7fe048b7df716b44490f946fe7613f70b0846c28
benjamin <2000beni770@gmail.com>
2025-08-19 20:47:23 +0300

    make dal handling id

--- services/dal.py
+++ services/dal.py
@@ -5,8 +5,8 @@ def insert_dic(dic):
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")  ## make connection
     mydb = myclient["enemy_soldiers"]  ## init the db
     mycol = mydb["soldier_details"]  ##init the collection
-    ##insert the first soldier
-    mycol.insert_one(dic)
+    sol_id  =mycol.insert_one(dic) ##insert the  soldier
+    return str(sol_id.inserted_id)
 
 
 def get_all():
@@ -14,7 +14,7 @@ def get_all():
     mydb = myclient["enemy_soldiers"]
     mycol = mydb["soldier_details"]
     res = []
-    for sol in mycol.find():
+    for sol in mycol.find({},{"_id":0}):
         res.append(sol)
     return res
 

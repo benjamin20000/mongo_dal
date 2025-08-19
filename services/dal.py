ee2e9e4fc7b5373416fbe98d05e6b5a7ec6921c6
benjamin <2000beni770@gmail.com>
2025-08-19 17:50:14 +0300

    updat dal

--- services/dal.py
+++ services/dal.py
@@ -1,18 +1,12 @@
 import pymongo
 
 
-def init_db_and_collection():
-    ##create the first soldier
-    first_soldier = { "first_name": "John","last_name":"John","phone_number":123, "rank":1 }
-    ##insert the first soldier
-    insert_soldier(first_soldier)
-
-def insert_soldier(soldier):
+def insert_dic(dic):
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")  ## make connection
     mydb = myclient["enemy_soldiers"]  ## init the db
     mycol = mydb["soldier_details"]  ##init the collection
     ##insert the first soldier
-    mycol.insert_one(soldier)
+    mycol.insert_one(dic)
 
 
 def get_all():

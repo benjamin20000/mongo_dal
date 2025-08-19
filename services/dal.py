836ff03d328438a87a9d7465ca166154e0bfd430
benjamin <2000beni770@gmail.com>
2025-08-19 17:23:00 +0300

    finc to insert soldier

--- services/dal.py
+++ services/dal.py
@@ -2,10 +2,15 @@ import pymongo
 
 
 def init_db_and_collection():
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["enemy_soldiers"] ## init the db
-    mycol = mydb["soldier_details"]  ##init the collection
     ##create the first soldier
     first_soldier = { "first_name": "John","last_name":"John","phone_number":123, "rank":1 }
     ##insert the first soldier
-    mycol.insert_one(first_soldier)
+    insert_soldier(first_soldier)
+
+def insert_soldier(soldier):
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")  ## make connection
+    mydb = myclient["enemy_soldiers"]  ## init the db
+    mycol = mydb["soldier_details"]  ##init the collection
+    ##insert the first soldier
+    mycol.insert_one(soldier)
+

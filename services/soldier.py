d8a6fb19ca74b4a718f346ff3616e00b055fee7b
benjamin <2000beni770@gmail.com>
2025-08-19 17:30:08 +0300

    add insert soldeir func

--- services/soldier.py
+++ services/soldier.py
@@ -1,9 +1,19 @@
-class Soldier:
+from dal import  insert_soldier
 
+
+class Soldier:
     def __init__(self , first_name , last_name , phone_number , rank, id = None):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.rank = rank
+
+    def insert_soldier_to_db(self):
+        soldier = {"first_name": self.first_name, "last_name": self.last_name, "phone_number": self.phone_number, "rank": self.rank}
+        insert_soldier(soldier)
+
+
+
+
 
